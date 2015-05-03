20685bade2c0f10baeb42f25d9fd2a95d6ed1eba
Ahmed Şeref <serefguneysu@gmail.com>
2015-05-03 13:41:15 +0300

    New SQL script showing latest Orders

---
+++ NorthWind-Sepet-Uygulamasi/Test.sql
@@ -0,0 +1,12 @@
+use Northwind
+DECLARE @OrderID int;
+SELECT TOP 1  @OrderID = OrderID  FROM ORDERS ORDER BY OrderID DESC
+SELECT O.OrderID, O.CustomerID, C.CompanyName, C.ContactName FROM [Orders] O
+	JOIN Customers C ON C.CustomerID = O.CustomerID
+WHERE OrderID = @OrderID
+SELECT OD.*, P.ProductName FROM [Order Details] OD
+	JOIN Products P ON P.ProductID = OD.ProductID
+WHERE OrderID = @OrderID 
+
+-- DELETE ORDERS; DELETE [ORDER DETAILS];
+-- SELECT COUNT(*) FROM ORDERS
